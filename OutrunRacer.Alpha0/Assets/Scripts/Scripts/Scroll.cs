8740cfd5d10aab5e20100a8e169059d699172559
PascalProulx <proulxpro@protonmail.com>
2020-03-19 12:58:31 -0400

    Test3

---
+++ OutrunRacer.Alpha0/Assets/Scripts/Scripts/Scroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Script that make a object scroll
+/// </summary>
+public class Scroll : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.transform.position += DummyCar.dummy.transform.forward * -0.1f;
+
+        if (DummyCar.currentPlatform == null) return;
+
+    }
+}
