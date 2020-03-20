3d13fa3dd8c9360916dfcf26021e4673b5172345
PascalProulx <proulxpro@protonmail.com>
2020-03-20 03:24:53 -0400

    jai refait le projet

---
+++ fuckmylife/Assets/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Animator anim;
+    public static GameObject player;
+    public static GameObject currentPlatform;
+    bool canTurn = false;
+    Vector3 startPosition;
+
+    void OnCollisionEnter(Collision other) {
+        currentPlatform = other.gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = this.GetComponent<Animator>();
+        player = this.gameObject;
+        startPosition = player.transform.position;
+        GenerateWorld.RunDummy();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other is BoxCollider && GenerateWorld.lastPlatform.tag != "platformTSection")
+            GenerateWorld.RunDummy();
+
+        if (other is SphereCollider)
+            canTurn = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other is SphereCollider)
+            canTurn = false;
+    }
+
+    void StopJump() {
+        anim.SetBool("isJumping", false);
+    }
+
+    void StopMagic() {
+        anim.SetBool("isMagic", false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool("isMagic") == false) {
+            anim.SetBool("isJumping", true);
+        } else if (Input.GetKeyDown(KeyCode.M) && anim.GetBool("isJumping") == false) {
+            anim.SetBool("isMagic", true);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow) && canTurn) {
+            this.transform.Rotate(Vector3.up * 90);
+            GenerateWorld.dummyTraveller.transform.forward = -this.transform.forward;
+            GenerateWorld.RunDummy();
+
+            if (GenerateWorld.lastPlatform.tag != "platformTSection")
+                GenerateWorld.RunDummy();
+
+            this.transform.position = new Vector3(startPosition.x,
+                                            this.transform.position.y,
+                                            startPosition.z);
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) && canTurn) {
+            this.transform.Rotate(Vector3.up * -90);
+            GenerateWorld.dummyTraveller.transform.forward = -this.transform.forward;
+            GenerateWorld.RunDummy();
+
+            if (GenerateWorld.lastPlatform.tag != "platformTSection")
+                GenerateWorld.RunDummy();
+
+            this.transform.position = new Vector3(startPosition.x,
+                                this.transform.position.y,
+                                startPosition.z);
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            this.transform.Translate(-0.5f, 0, 0);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            this.transform.Translate(0.5f, 0, 0);
+        }
+    }
+}
---
+++ fuckmylife/Assets/sources/Scripts/GenerateWorld.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Script that generate the game world
+/// </summary>
+public class GenerateWorld : MonoBehaviour
+{
+    static public GameObject dummyTraveller;    // Static gameObject that represent the dummy
+    static public GameObject lastPlatform;      // Static gameObject that represent the last platform that the player was
+
+    void Awake()
+    {
+        dummyTraveller = new GameObject("dummy");
+    }
+
+    /// <summary>
+    /// Function that generate the world 
+    /// </summary>
+    public static void RunDummy()
+    {
+        GameObject p = Pool.singleton.GetRandom();
+        if (p == null) return;
+        // Spawn the world in front of the player if he on a T-style platform
+        if (lastPlatform != null)
+        {
+            if(lastPlatform.tag == "platformTSection")
+                dummyTraveller.transform.position = lastPlatform.transform.position +
+                    CarController.player.transform.forward * 20;
+            else
+                dummyTraveller.transform.position = lastPlatform.transform.position +
+                    CarController.player.transform.forward * 24.2f;
+
+            if (lastPlatform.tag == "roadUp")
+                dummyTraveller.transform.Translate(0, 1.932f, 0);
+        }
+
+        lastPlatform = p;
+        p.SetActive(true);
+        p.transform.position = dummyTraveller.transform.position;
+        p.transform.rotation = dummyTraveller.transform.rotation;
+
+        if (p.tag == "roadDown")
+        {
+            dummyTraveller.transform.Translate(0, -1.932f, 0);
+            p.transform.Rotate(0, 180, 0);
+            p.transform.position = dummyTraveller.transform.position;
+        }
+
+        if (p.tag == "RightCurve")
+        {
+            p.transform.Rotate(0, 180, 0);
+            p.transform.position = dummyTraveller.transform.position;
+        }
+    }
+
+}
